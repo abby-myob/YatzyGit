ce1c17f131c77138bb022e9703f600baa189b130
Abby Thompson <abby.thompson@myob.com>
2019-08-14 09:26:40 +1200

    test for lenght of dicelist

--- Yatzy/YatzyTests/ScoringTests.cs
+++ Yatzy/YatzyTests/ScoringTests.cs
@@ -18,8 +18,21 @@ namespace YatzyTests
             {
                 Assert.Equal(1, die.Value);
             }
+        }
+
+        [Fact]
+        public void Check_dice_list_is_always_length_5()
+        {
+            var dice = new Dice();
+
+            List<Die> list = dice.GetDice();
+            
+            Assert.Equal(5, list.Count); 
         } 
-        
+    }
+
+    public class ScoringTests
+    {
         
     }
 }
