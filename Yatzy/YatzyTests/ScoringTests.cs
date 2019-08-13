37eb4d175b2bd5f9115bf90e1b63a6b50cb5ff41
Abby Thompson <abby.thompson@myob.com>
2019-08-14 09:38:23 +1200

    first scoring test

--- Yatzy/YatzyTests/ScoringTests.cs
+++ Yatzy/YatzyTests/ScoringTests.cs
@@ -4,13 +4,12 @@ using YatzyLibrary;
 
 namespace YatzyTests
 {
-
     public class DiceTests
     {
         [Fact]
         public void Get_dice_list_check_value_to_be_1()
         {
-            var dice = new Dice(); 
+            var dice = new Dice();
 
             List<Die> list = dice.GetDice();
 
@@ -26,13 +25,28 @@ namespace YatzyTests
             var dice = new Dice();
 
             List<Die> list = dice.GetDice();
-            
-            Assert.Equal(5, list.Count); 
-        } 
+
+            Assert.Equal(5, list.Count);
+        }
     }
 
     public class ScoringTests
     {
-        
+        [Theory]
+        [InlineData(new[]{2, 2, 2, 2, 2}, 10)]
+        public void Return_score_for_chance(int[] dice, int expected)
+        {
+            Score score = new Score();
+
+            Assert.Equal(expected, score.Chance(dice));
+        }
+    }
+
+    public class Score
+    {
+        public int Chance(int[] dice)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
